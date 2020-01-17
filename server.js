721ed16cdea5112c0b4d4c958e7dd2d4b16618dc
System Administrator <root@sundararajans-MacBook-Air.local>
2020-01-17 09:10:36 +0530

    Modifies server.js file

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const mongoose = require('mongoose');
 const chalk = require('chalk');
 const port = process.env.PORT || 5000;
 const app = express();
+app.use(cors());
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
 //server
 app.listen(port,() => {
   console.log(chalk.magentaBright('App running at port ',port));
